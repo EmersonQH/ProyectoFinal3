a5a1c3956af59dbda76a7d555b9172312f09da49
henry-uriel-bustinza <66810830+henry-uriel-bustinza@users.noreply.github.com>
2022-12-09 16:22:37 -0500

    Delete app directory

--- app/src/main/java/com/example/proyectofinal1/Presenter/LoginPresenter.java
+++
@@ -1,9 +0,0 @@
-package com.example.proyectofinal1.Presenter;
-
-public interface LoginPresenter {
-
-    void validateCredentials(String username, String password);
-
-
-    void onDestroy();
-}
--- app/src/main/java/com/example/proyectofinal1/View/CallFragment.java
+++
@@ -1,7 +0,0 @@
-package com.example.proyectofinal1.View;
-
-public interface CallFragment {
-
-    void changeFragment();
-
-}
